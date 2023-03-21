d999c5b92bb2e50720712573bb6aa1f2bcbf48fb
aberguecio1 <48263883+aberguecio1@users.noreply.github.com>
2023-03-21 19:39:26 -0300

    pagination

--- src/App.js
+++ src/App.js
@@ -1,45 +1,62 @@
-import React, { useState} from 'react';
+import React, { useState, useEffect} from 'react';
 import './App.css';
 
-function MyButton() {
-  const [trays, setTrays] = useState(true);
+function App() {
+  const [trays, setTrays] = useState([]);
+  const [returntrays, setReturntrays] = useState([]);
+  const [page, setPage] = useState(1);
+
   async function HandleClick(){
-    await fetch('https://tarea-1.2023-1.tallerdeintegracion.cl/trays', {method: "GET"})
+    setPage(page+1)
+    await fetch(`https://tarea-1.2023-1.tallerdeintegracion.cl/trays?page=${page}`, {method: "GET"})
     .then((response) => response.json())
     .then((data) => {
-      setTrays(data);
+      setReturntrays(data);
     });
   }
-  /* console.log(Object.values(trays)[0]) */
-  return (
-  <div>
-    {Object.values(trays)[0] ? (
-      Object.values(trays)[0].map(tray => {
-        return(
-            <div key={tray.id}>
-              <h3> Nombre: {tray.name}</h3>
-              <h3>Precio: {tray.price}</h3>
-            </div>
-        );
-      })
-    ) : (
-      <button onClick={HandleClick} className = "btn">
-      Click me
-    </button>
-    )}
-  </div>
-  );
-}
-
-function App() {
+  useEffect(() => {Object.values(returntrays)[0] && setTrays(trays.concat(Object.values(returntrays)[0]));
+    console.log(returntrays);
+  },[returntrays]);
   return (
     <div className="App">
       <header className="App-header">
         <h1 className = "Title">EL Puerquito</h1>
         <img src={require("./puerco.png")} className="App-logo" alt="logo" />
-        <MyButton/>
       </header>
+      {trays.length > 0 && (
+        <table id= "table">
+          <tr >
+            <th>Nombre</th>
+            <th>Precio</th>
+          </tr>
+        </table>
+      )}
+      {trays.length > 0 && (
+          trays.map(tray => {
+            return(
+              <table id= "table">
+                <tr key={tray.id}>
+                  <td>{tray.name}</td>
+                  <td>{tray.price}</td>
+                </tr>
+              </table>
+            );
+          })
+        )
+      }
+      <MyButton onClick={HandleClick}/>
     </div>
+  );
+}
+
+function MyButton({onClick}) {
+  /* console.log(Object.values(trays)[0]) */
+  return (
+  <div>
+    <button onClick={onClick} className = "btn">
+      Click me
+    </button>
+  </div>
   );
 }
 
