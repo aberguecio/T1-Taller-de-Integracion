4e59a76d23887eb83097d2825e2b84def1b5c437
aberguecio1 <48263883+aberguecio1@users.noreply.github.com>
2023-03-26 20:07:08 -0300

    searchbar

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 https://main--candid-pudding-b9a2d3.netlify.app/
+https://tarea-1.2023-1.tallerdeintegracion.cl/docs
--- src/App.js
+++ src/App.js
@@ -5,23 +5,24 @@ import Modal  from "./Modal.js"
 
 function App() {
   const [consult, setConsult] = useState("trays");
-  const [trays, setTrays] = useState([]);
-  const [returntrays, setReturntrays] = useState([]);
+  const [data, setData] = useState([]);
+  const [returndata, setReturndata] = useState([]);
   const [page, setPage] = useState(1);
+  const [sort, setSort] = useState("name");
+  const [order, setOrder] = useState("asc");
 
   const [show, setShow] = useState(false);
   const [selectedData, setSelectedData] = useState({});
 
+  const [searchText, setSearchText] = useState('');
 
-  function hideModal() {
-    setShow(false);
-  };
 
   async function Makecall(){
-    await fetch(`https://tarea-1.2023-1.tallerdeintegracion.cl/${consult}?sort=name&order=asc&page=${page}&size=25`, {method: "GET"})
+    await fetch(`https://tarea-1.2023-1.tallerdeintegracion.cl/${consult}?sort=${sort}&order=${order}&page=${page}&size=25`, {method: "GET"})
     .then((response) => response.json())
     .then((data) => {
-      setReturntrays(data);
+      console.log(data)
+      setReturndata(data);
     });
   }
 
@@ -31,20 +32,32 @@ function App() {
   },[page]);
 
   useEffect(() => {
-    setTrays([]);
+    setData([]);
     setPage(1)
     page === 1 && Makecall()
       // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[consult]);
+  },[consult,sort,order]);
 
-  useEffect(() => {Object.values(returntrays)[0] && setTrays(trays.concat(Object.values(returntrays)[0]));
-    console.log(returntrays);
+  useEffect(() => {Array.isArray(Object.values(returndata)[0]) ? (setData(data.concat(Object.values(returndata)[0]))):
+    (setData(data.concat(Object.values(returndata))));
+    console.log("-----", returndata, Object.values(returndata), Object.values(returndata)[0]);
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[returntrays]);
+  },[returndata]);
 
-  function HandleClick(){
+  function moreClick(){
     setPage(page+1)
   }
+
+  function hideModal() {
+    setShow(false);
+  };
+  function sortClick(){
+    sort === "name"? (setSort("price")):(setSort("name"))
+  }  
+  function orderClick(){
+    order === "asc"? (setOrder("desc")):(setOrder("asc"))
+  }
+
 
   async function TableClick(id){
     await fetch(`https://tarea-1.2023-1.tallerdeintegracion.cl/${consult}/${id}`, {method: "GET"})
@@ -56,6 +69,26 @@ function App() {
     });
   }
 
+  async function handleInputChange() {
+    await fetch(`https://tarea-1.2023-1.tallerdeintegracion.cl/search/${consult}?name=${searchText}`, {method: "GET"})
+    .then((response) => response.json())
+    .then((data) => {
+      setData([]);
+      console.log(data)
+      setReturndata(data);
+    });
+  }
+
+  function searchInput(event) {
+    setSearchText(event.target.value);
+    handleInputChange()
+  }
+
+  useEffect(() => {
+    handleInputChange()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[searchText]);
+
   return (
     <div className="App">
       <Appnavbar seter = {setConsult}/>
@@ -63,10 +96,18 @@ function App() {
         <h1 className = "Title">Le Puerquito</h1>
         <img src={require("./puerco.png")} className="App-logo" alt="logo" />
       </header>
+      <div>
+        <label>
+          Search:
+          <input type="text" value={searchText} onChange={searchInput} />
+        </label>
+      </div>
+      <MyButton onClick={sortClick} text="sort"/>
+      <MyButton onClick={orderClick} text="order"/>
 
       {show && <Modal details={selectedData} handleClose={hideModal} />}
 
-      {trays.length > 0 && (
+      {data.length > 0 && (
         <table className= "table">
           <tbody>
             <tr >
@@ -77,8 +118,8 @@ function App() {
           </tbody>
         </table>
       )}
-      {trays.length > 0 && (
-          trays.map(tray => {
+      {data.length > 0 && (
+          data.map(tray => {
             return(
               <> 
               <table className= "table" key={tray.id}>
@@ -98,19 +139,17 @@ function App() {
           })
         )
       }
-      <MyButton onClick={HandleClick}/>
+      <MyButton onClick={moreClick} text = "More"/>
     </div>
   );
 }
 
-function MyButton({onClick}) {
-  /* console.log(Object.values(trays)[0]) */
+function MyButton({onClick, text}) {
+  /* console.log(Object.values(data)[0]) */
   return (
-  <div>
     <button onClick={onClick} className = "btn">
-      More
+      {text}
     </button>
-  </div>
   );
 }
 
